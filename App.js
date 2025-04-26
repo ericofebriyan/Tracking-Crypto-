881f1bc76c1c2c5dfe734b886dcc6c6a59901dc6
erico febriyan <ericofebriyan90@gmail.com>
2025-04-26 16:32:43 +0700

    membuat folder screensdan components.serta membuat kode di app.js

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AllCurrenciesScreen from './screens/AllCurrenciesScreen';
+import DetailScreen from './screens/DetailScreen';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Detail" 
+          component={DetailScreen} 
+          options={{ title: 'Coin Detail', headerStyle: { backgroundColor: '#1A1A2E' }, headerTintColor: '#FFF' }} 
+        />
+        <Stack.Screen 
+          name="Currencies" 
+          component={AllCurrenciesScreen} 
+          options={{ title: 'Currencies', headerStyle: { backgroundColor: '#1A1A2E' }, headerTintColor: '#FFF' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
